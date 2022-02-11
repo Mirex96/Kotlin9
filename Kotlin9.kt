ee25f6ab90198823f117cf51f3212f627bca5a7b
Dyachenko Stanislav <boar_100@mail.ru>
2022-02-11 16:53:55 +0500

    v1

---
+++ Kotlin9.kt
@@ -0,0 +1,10 @@
+// 9. Написать функцию, который переворачивает строку: (String) -> String
+fun main() {
+    val str = "Good morning!"
+    coup(str)
+}
+
+fun coup(str: String) {
+    val coup = str.reversed()
+    println(coup)
+}       
